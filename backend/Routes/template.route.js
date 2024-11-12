0bb5b8a18e9dc46a1a6feb2dbc413bb38da671fb
tmasiwal <tanujmasiwal@gmail.com>
2024-11-12 11:10:39 +0530

    fix the error

--- backend/Routes/template.route.js
+++ backend/Routes/template.route.js
@@ -106,7 +106,7 @@ templateRouter.get('/contacts/:phone', templateController.getContactByPhone);
 templateRouter.get('/contacts/user/:userId', templateController.getContactsByUser);
 templateRouter.get('/all-attributes', templateController.getAllContactAttributesByUserId);
 templateRouter.get('/unique-attributes',templateController.getAllUniqueAttributes);
-templateRouter.get('/getBroadcast',templateController.getBroadcast);
+templateRouter.get('/getBroadcast/:userId',templateController.getBroadcast);
 // post api start 
 templateRouter.post('/createtemplete', templateController.createTemplate);
 templateRouter.post('/schedule', templateController.scheduleTemplate);
